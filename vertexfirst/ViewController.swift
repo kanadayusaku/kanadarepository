02ee95c269e1e44ae0824523cd2f2f198e544c46
kanadayusaku <38823610+kanadayusaku@users.noreply.github.com>
2019-08-04 08:50:35 +0900

    Initial Commit

---
+++ vertexfirst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vertexfirst
+//
+//  Created by 金田祐作 on 2019/08/04.
+//  Copyright © 2019 金田祐作. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
